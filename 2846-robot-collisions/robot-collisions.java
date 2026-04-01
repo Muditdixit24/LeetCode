434986bbc72c21ef642566f5f6b52cc89a17349d
Muditdixit24 <muditdxt24@gmail.com>
2026-04-01 17:55:36 +0530

    Time: 44 ms (61.74%) | Memory: 123.9 MB (47.83%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
         int n = positions.length;
-        // Pair robots with their index so we can sort by position
+        
         int[][] robots = new int[n][2];
         for (int i = 0; i < n; i++) {
             robots[i][0] = positions[i];
             robots[i][1] = i;
         }
-        Arrays.sort(robots, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(robots, (a, b) -> Integer.compare(a[0],
+         b[0]));
 
-        Stack<Integer> stack = new Stack<>(); // stores indices of robots moving right
+        Stack<Integer> stack = new Stack<>(); 
         boolean[] alive = new boolean[n];
         Arrays.fill(alive, true);
 
@@ -22,25 +23,25 @@ class Solution {
             if (dir == 'R') {
                 stack.push(idx);
             } else {
-                // Robot moving left collides with stack robots moving right
+                
                 while (!stack.isEmpty() && healths[idx] > 0) {
                     int rightIdx = stack.peek();
                     if (healths[rightIdx] < healths[idx]) {
-                        // Right robot dies
+                        
                         alive[rightIdx] = false;
                         stack.pop();
-                        healths[idx]--; // left robot loses 1 health
+                        healths[idx]--; 
                     } else if (healths[rightIdx] == healths[idx]) {
-                        // Both die
+                        
                         alive[rightIdx] = false;
                         alive[idx] = false;
                         stack.pop();
                         healths[idx] = 0;
                         break;
                     } else {
-                        // Left robot dies
+                        
                         alive[idx] = false;
-                        healths[rightIdx]--; // right robot loses 1 health
+                        healths[rightIdx]--; 
                         healths[idx] = 0;
                         break;
                     }
