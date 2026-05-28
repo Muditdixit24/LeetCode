8543e1d4df31ebc0f917b127c3b1acd278c3adb0
Muditdixit24 <muditdxt24@gmail.com>
2026-05-28 22:39:24 +0530

    Time: 8 ms (50.95%) | Memory: 46.1 MB (56.16%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String>set=new HashSet<>(wordDict);
+        boolean[]dp=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j]){
+                    String remaining=s.substring(j,i);
+                    if(set.contains(remaining)){
+                        dp[i]=true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.length()];
+        
+    }
+}
